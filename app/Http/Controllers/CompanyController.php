db94ecd44fa0e1cc12ed0ae59f0a7d20cc74183d
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-12-14 09:49:29 +0330

    handle logo uploading for company creation

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rules\File;
+use Intervention\Image\Laravel\Facades\Image;
 
 class CompanyController extends Controller
 {
@@ -30,9 +32,43 @@ class CompanyController extends Controller
             'xAccount' => ['bail', 'nullable', 'string', 'url', 'max:255'],
         ]);
 
-        $company = collect($validated)->except('logo')->all();
+        $logo_path = null;
 
-        Auth::user()->company()->create($company);
+        if (request()->hasFile('logo')) {
+            $file = request()->file('logo');
+
+            $image = Image::read($file->getRealPath());
+
+            $image->cover(300, 300);
+
+            $encodedImage = $image->toWebp(80);
+
+            $fileName = 'company_logo_'.time().'.webp';
+            $logo_path = 'logos/'.$fileName;
+
+            if (! Storage::put($logo_path, $encodedImage)) {
+                return back()->with('flashMessage', [
+                    'type' => 'error',
+                    'text' => 'Failed to save the logo.',
+                ]);
+            }
+        }
+
+        $company = Auth::user()->company()->create([
+            ...collect($validated)->except('logo')->all(),
+            'logo_url' => $logo_path,
+        ]);
+
+        if (! $company) {
+            if ($logo_path) {
+                Storage::delete($logo_path);
+            }
+
+            return back()->with('flashMessage', [
+                'type' => 'error',
+                'text' => 'Failed to save the company.',
+            ]);
+        }
 
         Auth::user()->update([
             'onboarding_completed' => true,
