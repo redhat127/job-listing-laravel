cda393e400c17221fed666cef520f79e3ef0a834
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-12-14 10:00:20 +0330

    remove all dirs from FILESYSTEM_DISK when seeding database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $directories = Storage::directories();
+
+        foreach ($directories as $directory) {
+            Storage::deleteDirectory($directory);
+        }
+
         User::factory()->create([
             'name' => 'dave',
             'email' => 'dave@example.com',
