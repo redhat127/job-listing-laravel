617da9b87e2bd8d77bc796f4c2d47d7832603a99
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-12-15 09:39:54 +0330

    add controlled state to user dropdown and prep for menu items

--- resources/js/components/user-dropdown.tsx
+++ resources/js/components/user-dropdown.tsx
@@ -1,15 +1,26 @@
 import type { User as IUser } from '@/types';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { LogoutForm } from './form/logout-form';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from './ui/dropdown-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
 import { UserAvatar } from './user-avatar';
 
-export const UserDropdown = ({ auth: { name, email, avatar } }: { auth: IUser }) => {
+export const UserDropdown = ({ auth: { name, email, avatar } }: { auth: Pick<IUser, 'name' | 'email' | 'avatar'> }) => {
   const userInitials = useMemo(() => {
     return <UserAvatar name={name} avatar={avatar} />;
   }, [name, avatar]);
+  const [open, setOpen] = useState(false);
+  //   const closeDropdown = useCallback(() => {
+  //     setOpen(false);
+  //   }, []);
   return (
-    <DropdownMenu>
+    <DropdownMenu open={open} onOpenChange={setOpen}>
       <DropdownMenuTrigger>{userInitials}</DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuLabel className="flex items-center gap-2">
@@ -19,6 +30,10 @@ export const UserDropdown = ({ auth: { name, email, avatar } }: { auth: IUser })
             <span className="truncate text-sm font-normal text-muted-foreground">{email}</span>
           </div>
         </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {/* TODO: don't know how this goes yet! */}
+        {/* <UserDropdownMenuItem href={home.url()} label="Favorite Jobs" icon={<Heart />} closeDropdown={closeDropdown} />
+        <UserDropdownMenuItem href={home.url()} label="My Jobs" icon={<Layers />} closeDropdown={closeDropdown} /> */}
         <DropdownMenuItem className="p-0">
           <LogoutForm />
         </DropdownMenuItem>
@@ -26,3 +41,14 @@ export const UserDropdown = ({ auth: { name, email, avatar } }: { auth: IUser })
     </DropdownMenu>
   );
 };
+
+// const UserDropdownMenuItem = ({ href, label, icon, closeDropdown }: { href: string; label: string; icon: ReactNode; closeDropdown(): void }) => {
+//   return (
+//     <DropdownMenuItem className="p-0">
+//       <Link href={href} className="flex w-full items-center gap-1.5 px-2 py-1.5" onClick={closeDropdown}>
+//         {icon}
+//         {label}
+//       </Link>
+//     </DropdownMenuItem>
+//   );
+// };
